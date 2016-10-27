add8eb83d1624e216568b2c2b2c9978be095920a
ARikka <anand.rikka@outlook.com>
2016-10-27 22:08:02 +0530

    let issue on server fixed

--- server/routes/movies.js
+++ server/routes/movies.js
@@ -6,12 +6,10 @@ var tmdbRequest = require('../tmdb/tmdb-request')('a7109af86b1d09cb5ae4087d83498
 
 router.get('/search', function (req, res) {
     tmdbRequest.searchMovie().then(function (result) {
-        let {config, headers, data} = result
-        res.send({config, headers, data}); 
+        res.send({config: result.config, headers: result.headers, data: result.data}); 
     }, function (error) {
         res.send(error)     
     })
-    
 });
 
 module.exports = router;
