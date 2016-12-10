14841d8d6c0369df000c2adaae28158ec29ad9ab
ARikka <anand.rikka@outlook.com>
2016-12-11 00:21:24 +0530

    removed express-session

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var app = express();
-var session = require('express-session');
 var logger = require('morgan');
 var jwt = require('jsonwebtoken');
 var Cookies = require('cookies');
